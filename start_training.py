95b35ecd36e0f54e015738c6c8cf14340bd70ad1
srinivasaguru1 <srinivasaguru301@gmail.com>
2025-04-29 20:07:08 +0530

    training master file updated

---
+++ start_training.py
@@ -0,0 +1,18 @@
+import os
+import sys
+
+from houseprice.exception.exception import HousePriceException
+from houseprice.logger.logger import logging
+
+from houseprice.pipeline.training_pipeline import TrainingPipeline
+
+
+def start_training():
+    try:
+        pass
+    except Exception as e:
+        raise HousePriceException(e,sys)
+    
+
+if __name__=='__main__':
+    start_training()
